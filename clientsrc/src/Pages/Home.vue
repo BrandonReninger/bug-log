9cc97e7b35128799d6f24b66523a246e286aacb7
Brandon Reninger <brandonreninger@yahoo.com>
2020-04-26 22:10:45 -0600

    bugs on home page not quite rendering

--- clientsrc/src/Pages/Home.vue
+++ clientsrc/src/Pages/Home.vue
@@ -1,13 +1,30 @@
 <template>
-  <div class="home">
+  <div class="home container-fluid">
     <h1 class="text-center">Welcome to Bug Tracker</h1>
     <create-bug></create-bug>
-    <bug></bug>
+    <div class="row d-flex">
+      <table class="col-12">
+        <thead class="bg-info mt-2">Current Bugs</thead>
+        <th class="col-3">Title</th>
+        <th class="col-3">Reported By</th>
+        <th class="col-2">Status</th>
+        <th class="col-3">Last Modified</th>
+        <tbody>
+          <tr v-for="bug in bugs" :key="bug.id" :bugData="bug">
+            <td class="col-3">{{bug.title}}</td>
+            <td class="col-3">{{bug.creatorEmail}}</td>
+            <td class="col-2">{{bug.closed}}</td>
+            <td class="col-3">{{bug.updatedAt}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
 import CreateBug from "../components/CreateBug";
+import Bug from "../components/Bug";
 export default {
   name: "home",
   data() {
@@ -21,6 +38,6 @@ export default {
       return this.$store.state.bugs;
     }
   },
-  components: { CreateBug }
+  components: { CreateBug, Bug }
 };
 </script>
---
+++ clientsrc/src/components/Bug.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bug container-fluid">
+    <h3>Current Bugs</h3>
+    <tr class="row">
+      <th class="col-3">{{bugData.title}}</th>
+      <th class="col-3">{{bugData.creatorEmail}}</th>
+      <th class="col-1">{{bugData.closed}}</th>
+      <th class="col-2">{{bugData.updatedAt}}</th>
+    </tr>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "bug",
+  props: ["bugData"],
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
