f7cdf52f9aa902ef85fc7f7dbcea857b342497ff
Brandon Reninger <brandonreninger@yahoo.com>
2020-04-30 00:04:53 -0600

    started some cosmetic stuff

--- clientsrc/src/Pages/Home.vue
+++ clientsrc/src/Pages/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home container-fluid">
-    <h1 class="text-center">Welcome to Bug Tracker</h1>
+    <h1 class="text-center">Welcome to BugTrakker</h1>
     <create-bug></create-bug>
 
-    <table class="table">
-      <thead class="bg-info">Current Bugs</thead>
-      <th scope="col">Title</th>
-      <th scope="col">Reported By</th>
-      <th scope="col">Status</th>
-      <th scope="col">Last Modified</th>
-      <tbody>
-        <bug v-for="bug in bugs" :key="bug._id" :bugData="bug"></bug>
-      </tbody>
-    </table>
+    <div class="container table-responsive">
+      <table class="table table-striped mx-auto w-auto">
+        <thead class="bg-info">Current Bugs</thead>
+        <th>Title</th>
+        <th>Reported By</th>
+        <th>Status</th>
+        <th>Last Modified</th>
+        <tbody>
+          <bug v-for="bug in bugs" :key="bug._id" :bugData="bug"></bug>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
